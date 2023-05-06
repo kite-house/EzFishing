02590abdb4f1cd52a0024d3cc968dde893180aa1
Kite-house <hodosok1301@gmail.com>
2023-05-06 09:01:11 +0700

    rel

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import pyuac
 import pyautogui
 import keyboard
 import time
@@ -6,28 +5,30 @@ import random
 from bin import detected_fish
 from datetime import datetime 
 
-if not pyuac.isUserAdmin():
-    pyuac.runAsAdmin()
-
 print(f'{datetime.now().replace(microsecond=0)} || BOT loading. Please wait 5 second!')
 time.sleep(5)
 
-while True:
-    print(f'{datetime.now().replace(microsecond=0)} || Start catch fish!')
-    keyboard.press_and_release('e')
-    time.sleep(0.2)
-    keyboard.press_and_release('`')
+try:
 
-    detected_fish.detected()
-    print(f'{datetime.now().replace(microsecond=0)} || Pulling Out fish!')
-    pyautogui.mouseDown(button='left')
-    time.sleep(15)
-    pyautogui.mouseUp(button='left')
+    while True:
+        print(f'{datetime.now().replace(microsecond=0)} || Start catch fish!')
+        keyboard.press_and_release('e')
+        time.sleep(0.2)
+        keyboard.press_and_release('`')
 
-    print(f'{datetime.now().replace(microsecond=0)} || EZ FISH!!!')
-    pause = random.random().__round__(1) + random.random().__round__(1)
-    if pause < 0.5:
-        pause = 0.5
-    time.sleep(0.1)
-    keyboard.press_and_release('`')
-    time.sleep(pause)
+        detected_fish.detected()
+        print(f'{datetime.now().replace(microsecond=0)} || Pulling Out fish!')
+        pyautogui.mouseDown(button='left')
+        time.sleep(15)
+        pyautogui.mouseUp(button='left')
+
+        print(f'{datetime.now().replace(microsecond=0)} || EZ FISH!!!')
+        pause = random.random().__round__(1) + random.random().__round__(1)
+        if pause < 0.5:
+            pause = 0.5
+        time.sleep(0.1)
+        keyboard.press_and_release('`')
+        time.sleep(pause)
+
+except Exception as error:
+    print(f'{datetime.now().replace(microsecond=0)} || ERROR: {error} || Try restarting the applications!')
